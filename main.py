f2f1ba0f1bba69ca7e231fda9e8da787275f1ac6
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-01-22 19:50:44 +0000

    add reconnection check

--- main.py
+++ main.py
@@ -24,45 +24,89 @@ def wifi_error_led_flash():
     blink_led(20, 100)
 
 
-def make_network_request():
-    Wifi.connect()
-    response = urequests.get(
-        'https://api.tfl.gov.uk/Line/central/Status?formatter=json', headers={"Accept": "application/json"})
-    print(response.json())
-    Wifi.disconnect()
-
-
-def check_awake_and_make_network_request():
-    is_awake_led_flash()
+def make_request():
     try:
-        make_network_request()
+        response = urequests.get(
+            'https://api.tfl.gov.uk/Line/central/Status?formatter=json', headers={"Accept": "application/json"})
+        print(response.json())
         is_connected_to_wifi_led_flash()
     except Exception as e:
         print(e)
         wifi_error_led_flash()
 
 
-while True:
+def connect_and_make_request():
+    try:
+        Wifi.connect()
+        make_request()
+    except Exception as e:
+        print(e)
+        wifi_error_led_flash()
 
-    # Awake state
-    check_awake_and_make_network_request()
 
-    # Transition state
-    time.sleep(1)
-    print('entering light sleep')
-    lightsleep(1000)
+def connect_and_make_request_and_disconnect():
+    connect_and_make_request()
+    Wifi.disconnect()
 
-    # Awoken state from light sleep
-    check_awake_and_make_network_request()
 
-    # Transition state
-    time.sleep(1)
-    print('entering deep sleep')
-    deepsleep(1000)
+def check_network_connectivity_without_reconnecting():
+    while True:
+        # Awake state
+        is_awake_led_flash()
+        connect_and_make_request()
 
-    # Awoken state from deep sleep
-    check_awake_and_make_network_request()
+        # Transition state
+        time.sleep(1)
+        print('entering light sleep')
+        lightsleep(1000)
 
-    # Loop preparation
-    time.sleep(1)
-    print('awoken, one more time!')
+        # Awoken state from light sleep
+        is_awake_led_flash()
+        make_request()
+
+        # Transition state
+        time.sleep(1)
+        print('entering deep sleep')
+        deepsleep(1000)
+
+        # Awoken state from deep sleep
+        # Awoken state from light sleep
+        is_awake_led_flash()
+        make_request()
+
+        # Loop preparation
+        time.sleep(1)
+        print('awoken, one more time!')
+
+
+def check_network_connectivity_with_reconnecting():
+    while True:
+        # Awake state
+        is_awake_led_flash()
+        connect_and_make_request_and_disconnect()
+
+        # Transition state
+        time.sleep(1)
+        print('entering light sleep')
+        lightsleep(1000)
+
+        # Awoken state from light sleep
+        is_awake_led_flash()
+        connect_and_make_request_and_disconnect()
+
+        # Transition state
+        time.sleep(1)
+        print('entering deep sleep')
+        deepsleep(1000)
+
+        # Awoken state from deep sleep
+        # Awoken state from light sleep
+        is_awake_led_flash()
+        connect_and_make_request_and_disconnect()
+
+        # Loop preparation
+        time.sleep(1)
+        print('awoken, one more time!')
+
+
+check_network_connectivity_without_reconnecting()
