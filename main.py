3f295ce4dd3bc0a546f0ff3d170270598d50a0fb
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-01-22 19:34:54 +0000

    add example of light sleep and deep sleep

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
+from machine import Pin, lightsleep, deepsleep
+from wifi import Wifi
+import urequests
+import time
 
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
-from spatial import *
+led = Pin('LED', Pin.OUT)
 
-display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, rotate=90)
-width, height = display.get_bounds()
 
-display_cell = CellCreator.create_cell_from_display_dimensions(
-    width, height)
+def blink_led(times, interval):
+    for _ in range(times):
+        led.toggle()
+        time.sleep_ms(interval)
 
-white = display.create_pen(255, 255, 255)
-black = display.create_pen(0, 0, 0)
 
-display.set_pen(white)
-display.rectangle(0, 0, width, height)
+def is_awake_led_flash():
+    blink_led(6, 200)
 
-display.set_pen(black)
-subgrid = Grid.create_cell_grid_in_cell(display_cell, props=GridProps(
-    spacing_type='percentage_summation', x_spacing_values=[0.333, 0.666], y_spacing_values=[0], margin=5))
 
-for row in subgrid:
-    for cell in row:
-        display.rectangle(cell.left_bounds, cell.top_bounds,
-                          cell.width, cell.height)
+def is_connected_to_wifi_led_flash():
+    blink_led(4, 1000)
 
-display.update()
+
+def wifi_error_led_flash():
+    blink_led(20, 100)
+
+
+def make_network_request():
+    Wifi.connect()
+    response = urequests.get(
+        'https://api.tfl.gov.uk/Line/central/Status?formatter=json', headers={"Accept": "application/json"})
+    print(response.json())
+    Wifi.disconnect()
+
+
+def check_awake_and_make_network_request():
+    is_awake_led_flash()
+    try:
+        make_network_request()
+        is_connected_to_wifi_led_flash()
+    except Exception as e:
+        print(e)
+        wifi_error_led_flash()
+
+
+while True:
+
+    # Awake state
+    check_awake_and_make_network_request()
+
+    # Transition state
+    time.sleep(1)
+    print('entering light sleep')
+    lightsleep(1000)
+
+    # Awoken state from light sleep
+    check_awake_and_make_network_request()
+
+    # Transition state
+    time.sleep(1)
+    print('entering deep sleep')
+    deepsleep(1000)
+
+    # Awoken state from deep sleep
+    check_awake_and_make_network_request()
+
+    # Loop preparation
+    time.sleep(1)
+    print('awoken, one more time!')
--- wifi/services/wifi.py
+++ wifi/services/wifi.py
@@ -11,9 +11,13 @@ class Wifi:
         wlan = network.WLAN(network.STA_IF)
         wlan.active(True)
         wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-        while wlan.isconnected() == False:
+        attempt = 0
+        while wlan.isconnected() == False and attempt < 10:
             print('Waiting for connection...')
             sleep(1)
+            attempt += 1
+        if not wlan.isconnected():
+            raise Exception('Failed to connect to the network')
         print(f'Connected \nNetwork config: {wlan.ifconfig()}')
 
     @staticmethod
